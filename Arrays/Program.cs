a7b28a5d44c891d78e4283d572b1947d919fd3d2
Mert Abdullah Arslan <143932563+marslan231@users.noreply.github.com>
2024-06-05 21:08:50 +0300

    Add files via upload

---
+++ Arrays/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Sleep(int Sleeptime)
+        {
+            Thread.Sleep(Sleeptime);
+        }
+        static void Main(string[] args)
+        {
+            //string[] students = new string[3];
+            //students[0] = "Engin";
+            //students[1] = "Mert";
+            //students[2] = "Beyza";
+            //string[] students = new string[3] { "Mert", "Beyza", "Emine" };
+            //string[] students2 = { "Mert", "Beyza", "Zümra" };
+            //foreach (string student in students)
+            //{
+            //    Console.WriteLine(student);
+            //}
+            //foreach (string student in students2)
+            //{
+            //    Console.WriteLine(student);
+
+            //}
+
+            string[,] regions = new string[5, 3]
+            {
+                { "İstanbul","İzmit","Kocaeli" },
+                { "Ankara","Konya","Kırıkkale" },
+                { "Antalya","Adana","Mersin" },
+                { "Trabzon","Gireun","Samsun" },
+                { "İzmir","Muğla","Manisa" }
+            };
+
+            for (int i = 0; i <= regions.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(regions[i,j]);
+                }
+                Console.WriteLine("**********");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
